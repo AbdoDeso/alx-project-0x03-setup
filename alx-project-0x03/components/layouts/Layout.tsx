b2762f4530707b16b7ea2eb6c3a9c82951e19ab6
Abdullah Eldesoky <microstar747@gmail.com>
2025-07-02 01:26:02 +0300

    update

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -2,7 +2,7 @@
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
-import {LayoutProps} from "../../interface"
+import { LayoutProps } from "../../interface"
 
 
 
